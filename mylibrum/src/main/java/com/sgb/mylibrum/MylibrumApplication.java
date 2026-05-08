8f6c8da799b5fbd1a09c6a2888ae0ba6976760c7
Jmarcelosl <joao_m_lessa@estudante.sc.senai.br>
2026-05-08 20:00:02 -0300

    Estrutura inicial gerada pelo Spring Initializr

---
+++ mylibrum/src/main/java/com/sgb/mylibrum/MylibrumApplication.java
@@ -0,0 +1,13 @@
+package com.sgb.mylibrum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MylibrumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MylibrumApplication.class, args);
+	}
+
+}
---
+++ mylibrum/src/test/java/com/sgb/mylibrum/MylibrumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgb.mylibrum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MylibrumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
